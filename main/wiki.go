8842b9a98f6018318b4958e465c80a5c7ddf5168
Sid Saddala <sid.saddaal@capitalone.com>
2016-11-19 00:46:25 -0500

    new functionality

--- main/wiki.go
+++ main/wiki.go
@@ -3,11 +3,12 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"net/http"
 )
 
 type Page struct {
 	Title string
-	Body []byte
+	Body  []byte
 }
 
 func (p *Page) save() error {
@@ -15,18 +16,22 @@ func (p *Page) save() error {
 	return ioutil.WriteFile(filename, p.Body, 0600)
 }
 
-func loadPage (title string) (*Page, error) {
+func loadPage(title string) (*Page, error) {
 	filename := title + ".txt"
 	body, err := ioutil.ReadFile(filename)
-	if err !=nil {
+	if err != nil {
 		return nil, err
 	}
-	return &Page{Title: title, Body: body}
+	return &Page{Title: title, Body: body}, nil
 }
 
-func main(){
-	p1 := &Page{Title: "TestPage", Body: []byte("This is sample page")}
-	p1.save()
-	p2,_ := loadPage("TetPage")
-	fmt.Println(string(p2.Body))
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
+func main() {
+	http.HandleFunc("/view/", viewHandler)
+	http.ListenAndServe(":8080", nil)
 }
